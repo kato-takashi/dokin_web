d8dfdac69918b84385299ab458e90e4a99f7869b
Kato Takashi <taka4.kato@gmail.com>
2015-12-29 13:54:42 +0900

    アニメーションの実装完了

--- js/svg_set.js
+++ js/svg_set.js
@@ -1,29 +1,33 @@
 $(function(){
   // 一定期間繰り返し
-  setInterval(function(){ drawSvg(pathObj2); },3000);
-
+  // setInterval(function(){ drawSvg(); },3000);
+  setInterval(drawSvg,3000);
 });
 
 //senngawo 
-function drawSvg(pathObj){
-  $('#draw_svg').show();
-  $('#draw_svg').lazylinepainter( 
+function drawSvg(){
+
+  var rand = Math.floor( Math.random() * pathArray.length);
+  console.log(pathArray[rand]);
+  $(draw_svg_array[rand]).show();
+  $(draw_svg_array[rand]).fadeIn("500");
+  $(draw_svg_array[rand]).lazylinepainter( 
   {
-    "svgData": pathObj,
+    "svgData": pathArray[rand],
     'duration':300, // path duration
-    "strokeWidth": 2,　//線の太さ
+    "strokeWidth": 1,　//線の太さ
     "strokeColor": "#FFF",　//線の色
     'onStrokeComplete':  function(){
       //遅延処理
        setTimeout(function() {
               // 線画消して元のロゴフェードインする
-              drawComp();
+              drawComp(draw_svg_array[rand]);
         }, 1000);
     }
   }).lazylinepainter('paint');
 }
 
 //線が終了後の処理
-function drawComp(){
-  $('#draw_svg').fadeOut("2000");
+function drawComp(target){
+  $(target).fadeOut("2000");
 }
